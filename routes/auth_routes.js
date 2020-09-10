32ea16b8fbc68f46e208901f9b780b28c6c06ba5
treoa <anuar.suleimenov@nu.edu.kz>
2020-09-11 00:19:55 +0600

    connected app

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const user = require('../models/user')
+const config = require('config')
 const {check, validationResult} = require('express-validator')
 const router = Router()
 
@@ -13,6 +15,16 @@ router.post(
     ],
     async (req, res) => {
     try {
+
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty) {
+            return res.status(400)
+            .json({errors: errors.array(),
+                message: "Incorrect email or password"
+            })
+        }
+
         const {email, password} = req.body
         const candidate = await user.findOne({email})
         if (candidate) {
@@ -34,8 +46,51 @@ router.post(
 })
 
 // api/auth/login
-router.post('/login', async (req, res) => {
+router.post('/login', 
+    [
+        check('email', 'Entered incorrect email').normalizeEmail().isEmail().isEmail(),
+        check('password', 'Incorrect password entered').exists()
+    ],
+    async (req, res) => {
+        try {
+
+            const errors = validationResult(req)
     
+            if (!errors.isEmpty) {
+                return res.status(400)
+                .json({errors: errors.array(),
+                    message: "Incorrect email or password"
+                })
+            }
+
+            const {email, password} = req.body
+
+            const user = await user.findOne({email})
+
+            if (!user) {
+                return res.status(400)
+                    .json({message: 'The user not found'})
+            }
+
+            // HOW BCRYPT FINDS UNCRYPTED PASS AMONGST CRYPTED ONES?
+            const temp_pass = await bcrypt.compare(password, user.password)
+
+            if (!temp_pass) {
+                return res.status(400)
+                    .json({message: 'The user password is incorrect'})
+            }
+
+            const token = jwt.sign(
+                { userID: user.id },
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+
+            res.json({token, userID: user.id})
+    
+        } catch (e) {
+            res.status(500).json({message: 'Something went wrong in the response'})
+        }
 })
 
 module.exports = router
